1e6f55922ade4797e7e8a73bb06862c60a429045
Liu Zhenghua <1090179900@qq.com>
2022-03-29 17:22:16 +0800

    json util test

--- src/test/java/com/matic/algorithm/test/JsonUtilTest.java
+++ src/test/java/com/matic/algorithm/test/JsonUtilTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.matic.algorithm.test.model.User;
