ae23f32024080fd769f534c00ba7ede16b1b8d61
Liu Zhenghua <1090179900@qq.com>
2022-04-18 14:10:39 +0800

    json util add some method

--- src/main/java/com/zakiis/common/JsonUtil.java
+++ src/main/java/com/zakiis/common/JsonUtil.java
@@ -1,14 +1,19 @@
 package com.zakiis.common;
 
 import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -21,7 +26,7 @@ public class JsonUtil {
 		mapper
 			//.setVisibility(PropertyAccessor.ALL, Visibility.NONE)
 			//.setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
-			.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA))
+			.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
 			.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 			.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
@@ -52,5 +57,42 @@ public class JsonUtil {
 		}
 		return null;
 	}
+	
+	public static String getString(JsonNode node) {
+        if (node == null || node.isNull()) {
+            return null;
+        }
+        return node.asText(null);
+    }
+	
+	public static Date getDate(JsonNode node) {
+        if (node == null || node.isNull()) {
+            return null;
+        }
+        String dateStr = node.asText();
+        if (StringUtils.isEmpty(dateStr)) {
+        	return null;
+        }
+        try {
+			return mapper.getDateFormat().parse(dateStr);
+		} catch (Exception e) {
+			logger.error("to date error.", e);
+		}
+        return null;
+    }
+	
+	public static List<String> getStringList(JsonNode node) {
+        if (node == null || node.isNull() || !(node.isArray() || node.isTextual())) {
+            return null;
+        }
+        if (node.isTextual() && !node.asText().isEmpty()) {
+            return Collections.singletonList(node.asText());
+        }
+        List<String> list = new ArrayList<>(node.size());
+        for (int i = 0; i < node.size(); i++) {
+        	list.add(getString(node.get(i)));
+        }
+        return list;
+    }
 
 }
